c6dd5e7fca3d5f8fca8ce58980f62885fbebcd2e
yuluohuangquan <648779925@qq.com>
2019-08-11 16:28:01 +0800

    引入ts

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            welcome sun
+          </p>
+        </header>
+      </div>
+    );
+  };
+}
+
+export default App;

c29628c2d873d4a83939c995758fb00afbbb20dc
z004069p <yu.sun.ext@siemens.com>
2020-04-30 17:04:04 +0800

    登录页面

--- src/components/login/main.tsx
+++ src/components/login/main.tsx
@@ -1,20 +1,43 @@
 import React from "react";
-import { Button } from "antd";
+import { Button, Card, Input, Form } from "antd";
 import { useHistory } from "react-router-dom";
+import "./styles/main.modules.less";
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
 
 export default function Login() {
   let history = useHistory();
+  const [form] = Form.useForm();
+
+  const login = () => {
+    form.getFieldsError();
+    window.console.log(form.getFieldsError());
+    history.push("/translate");
+  };
 
   return (
-    <>
-      <p>{"登录页面"}</p>
-      <Button
-        onClick={() => {
-          history.push("/test");
-        }}
-      >
-        {"去测试页面"}
-      </Button>
-    </>
+    <div className={"login"}>
+      <Card title={"登录"} className={"login-main"}>
+        <Form form={form}>
+          <Form.Item
+            name="name"
+            rules={[{ required: true, message: "请输入用户名" }]}
+          >
+            <Input placeholder={"请输入用户名"} addonBefore={<UserOutlined />} />
+          </Form.Item>
+          <Form.Item
+            name="password"
+            rules={[{ required: true, message: "请输入密码" }]}
+          >
+            <Input.Password placeholder={"请输入密码"} addonBefore={<LockOutlined />} />
+          </Form.Item>
+        </Form>
+        <Button
+          className={"login-button"}
+          onClick={login}
+        >
+          {"登录"}
+        </Button>
+      </Card>
+    </div>
   );
-};
+}
